9f2a27d0ed952f3a48e40e963fc83e1124432bea
Bobby Durrett <bobby@bobbydurrettdba.com>
2016-03-30 09:19:29 -0700

    Added license block to source files.

--- myplot.py
+++ myplot.py
@@ -1,4 +1,21 @@
 """
+PythonDBAGraphs: Graphs to help with Oracle Database Tuning
+Copyright (C) 2016  Robert Taft Durrett (Bobby Durrett)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+myplot.py
 
 My plotting routines
 
